09878667faf1d692c055feaf6e5fefa41bcafff6
netryxdev <adrianoneto2501@gmail.com>
2021-11-28 23:12:10 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,18 @@
+<h3> Fornecedor</h3>
+
+{{-- Aqui fica o comentário que será descartado pelo interpretador do blade --}}
+
+{{-- @unless executa se o retorno for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status {{ $fornecedores[0]['status'] }}
+<br>
+@if( !($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
+@endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+    @endunless
+    <br>
